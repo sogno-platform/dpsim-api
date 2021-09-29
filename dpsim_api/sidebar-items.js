b05cb6863c400a008a6ada410ccf4f1c32bbf473
richardmarston <mail@richardmarston.net>
2021-09-29 10:32:27 +0000

    Build documentation: (14627f6) Use okapi to generate openapi/swagger
    Signed-off-by: Richard Marston <rmarston@eonerc.rwth-aachen.de>

--- dpsim_api/sidebar-items.js
+++ dpsim_api/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"enum":[["SimulationType",""]],"fn":[["main","The main entry point for Rocket"],["osstr_to_string",""],["parse_simulation_form",""],["read_zip",""]],"mod":[["amqp",""],["db",""],["routes",""]],"struct":[["Simulation",""],["SimulationForm","Form for submitting a new Simulation"]]});
+initSidebarItems({"enum":[["SimulationType","Enum for the various Simulation types"]],"fn":[["main","The main entry point for Rocket"]],"mod":[["amqp",""],["db",""],["routes",""]],"struct":[["Simulation","Struct for encapsulation Simulation details"]]});
