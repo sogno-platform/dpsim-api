89c1bff66bad5575959b3ddf72d4f86c04c6a52d
Richard Marston <rmarston@eonerc.rwth-aachen.de>
2022-05-18 14:14:26 +0200

    Reduce docker image size using build container
    Signed-off-by: Richard Marston <rmarston@eonerc.rwth-aachen.de>

--- scripts/build_docker.sh
+++ scripts/build_docker.sh
@@ -1,6 +1,14 @@
-!#/bin/bash
+#!/bin/bash
 
-docker build -t dpsim-api .
-docker tag dpsim-api:latest localhost:5000/dpsim-api
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )/..
+pushd $SCRIPT_DIR && echo "Changed to $SCRIPT_DIR"
+
+docker build -f Dockerfile -t dpsim-api . && \
+docker tag dpsim-api:latest localhost:5000/dpsim-api && \
 docker push localhost:5000/dpsim-api
 
+if [ "$?" != "0" ];
+then
+  sleep 20
+  docker push localhost:5000/dpsim-api
+fi
