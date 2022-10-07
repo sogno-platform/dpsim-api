36ef7d9248225831168b021c4704081d6e190f97
richardmarston <mail@richardmarston.net>
2022-10-07 13:53:30 +0000

    Build documentation: (e43c47c) Return array of SimulationSummary for /simulation endpoint
    Signed-off-by: Richard Marston <rmarston@eonerc.rwth-aachen.de>

--- source-files.js
+++ source-files.js
@@ -1,3 +1,4 @@
-var sourcesIndex = {};
-sourcesIndex["dpsim_api"] = {"name":"","files":["amqp.rs","db.rs","file_service.rs","main.rs","routes.rs"]};
+var sourcesIndex = JSON.parse('{\
+"dpsim_api":["",[],["amqp.rs","db.rs","file_service.rs","main.rs","routes.rs"]]\
+}');
 createSourceSidebar();
