a59589cb5ba20235d49070ed84c3db895129f39e
Richard Marston <rmarston@eonerc.rwth-aachen.de>
2022-10-12 09:23:05 +0200

    Make load profile data optional
    Signed-off-by: Richard Marston <rmarston@eonerc.rwth-aachen.de>

--- scripts/build_docker.sh
+++ scripts/build_docker.sh
@@ -3,9 +3,16 @@
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )/..
 pushd $SCRIPT_DIR && echo "Changed to $SCRIPT_DIR"
 
-docker build -f Dockerfile -t dpsim:api . && \
-docker tag dpsim:api localhost:5000/dpsim:api && \
-docker push localhost:5000/dpsim:api
+if [ "x$1" == "xdev" ]
+then
+	docker build -f Dockerfile.dev -t dpsim:api . && \
+	docker tag dpsim:api localhost:5000/dpsim:api && \
+	docker push localhost:5000/dpsim:api
+else
+	docker build -f Dockerfile -t dpsim:api . && \
+	docker tag dpsim:api localhost:5000/dpsim:api && \
+	docker push localhost:5000/dpsim:api
+fi
 
 if [ "$?" != "0" ];
 then
